5cc5265da27a6351a11250eb3981f868feab741c
Théo Feschotte <tf@esilab.fr>
2024-08-08 02:20:04 +0200

    Ajustement menu responsive

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,20 +1,32 @@
-import modular from "./modujs";
-import * as modules from "./modules";
-import globals from "./globals";
-import { html } from "./utils/environment";
+// import modular from "./modujs";
+// import * as modules from "./modules";
+// import globals from "./globals";
+// import { html } from "./utils/environment";
 
-const app = new modular({
-    modules: modules,
-});
+// const app = new modular({
+//     modules: modules,
+// });
 
 window.addEventListener("load", () => {
     init();
 });
 
 function init() {
-    html.classList.remove("no-js");
+    // html.classList.remove("no-js");
     
-    globals();
+    // globals();
     
-    app.init(app);
+    // app.init(app);
+
+    // Menu toggler
+    console.log("Menu init");
+    const Content = document.querySelector("[data-content]");
+    const MenuContainer = document.querySelector("[data-menu-container]");
+    const MenuToggle = document.querySelector("[data-menu-toggle]");
+    MenuToggle.addEventListener("click", () => {
+        Content.classList.toggle("o-oh");
+        MenuContainer.classList.toggle("c-menu--closed");
+        MenuToggle.classList.toggle("p-header__menu-toggle--active");
+    });
+
 };
